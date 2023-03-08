4ab46a59e2a9874b48d2cde0bca96ab57256aee4
Ethan Zhang <zhangeth@usc.edu>
2023-03-07 19:49:12 -0800

    added test logs

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -205,7 +205,7 @@ public class TableManagerImpl implements TableManager{
 
     tx.commit().join();
 
-    tableDir.remove(db);
+    rootDir.remove(db, PathUtil.from(tableName));
 
 /*    System.out.println("Printing remaining keys: " + listTables().size());
     for (String key : listTables().keySet())
